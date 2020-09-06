702b7b335bcb9aec8eb08f08902c48b7c6fa28c8
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-06 23:45:38 +0530

    Multiple Time Flip Singlet

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,35 @@
 #!/bin/bash -x
 
-	flip=$((RANDOM%2))
+	read -p "Enter How many times You Want to Flip Coin : " Total_Flips
 
-	if [ $flip -eq 0 ]
-	then
-		echo "Heads"
-	else
-		echo "Tails"
-	fi
+	declare -A Singlet
+
+	function persentage()
+	{
+		dict_keys=("$@")
+		for occur in ${dict_keys[@]}
+		do
+			persent=`echo ${Singlet[$occur]} $Total_Flips |awk '{print $1/$2*100} ' `
+			echo "Persentage of $occur = $persent"
+		done
+	}
+
+	heads=0
+	tails=0
+	for((i=1;i<=$Total_Flips;i++))
+	do
+
+		flip=$((RANDOM%2))
+
+		case $flip in
+
+		0) Singlet["H"]=$((++heads))			# 0 is for Heads
+		;;
+
+		1) Singlet["T"]=$((++tails))			# 1 is for Tails
+		;;
+
+	esac
+	done
+
+	persentage ${!Singlet[@]}
